4bc173f9a36c7111a966a36cca1ed13190bccbfa
nikolai-nik <nikolaivista@gmail.com>
2017-09-16 17:39:41 +0300

    upd: scripts

--- assets/js/site-scripts.js
+++ assets/js/site-scripts.js
@@ -3,13 +3,29 @@
 
 	$( document ).ready( function () {
 
-	$( '#fixed-menu-right' ).on('click', '.navigation-right', function( event ) {
-		var $target = $( event.currentTarget ),
-			$parent = $target.parent( '.navigation-right' );
+		$( '#fixed-menu-right' ).on('click', '.navigation-right', function( event ) {
+			var $target = $( event.currentTarget ),
+				$parent = $target.parent( '.navigation-right' );
 
 
 			$parent.toggleClass( 'active' );
-		})
-	});
+		});
+
+	// Here
+	
+	
+
+	$('div[data-type="background"]').each(function(){
+    	var $bgobj = $(this);
+
+ 		$( window ).scroll( function() {
+        	var yPos = -( $( window ).scrollTop() / $bgobj.data('speed')); 
+        	var coords = '50% '+ yPos + 'px';
+     
+        	$bgobj.css({ backgroundPosition: coords });
+    	} ); 
+	} );  
+
+	});// end ready
 
 } ( jQuery ) );
